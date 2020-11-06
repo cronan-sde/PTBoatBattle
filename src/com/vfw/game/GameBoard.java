e14172196a4ccd2cea1cc440f14503c1e18c2019
Cody Cronberger <codycronberger@gmail.com>
2020-11-06 14:11:03 -0800

    Added updateBoard, printBoard for GameBoard Class

--- src/com/vfw/game/GameBoard.java
+++ src/com/vfw/game/GameBoard.java
@@ -11,14 +11,13 @@ import java.util.TreeMap;
 class GameBoard {
     private char[][] board; // 2D game board
     private static final int SIZE = 10; // dimension of game board
+    // list of row identifiers
     private static final List<Character> CHAR_KEYS = new ArrayList<>(
             Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'));
 
-    // holds row char and its associated value for easy lookup in the 2D board array
+    // holds row identifier and its associated value for easy lookup in the 2D board array
     private Map<Character, Integer> rowKeyValues = new TreeMap<>();
 
-    // Symbols to represent board characters
-    // '@' = HumanPlayer ships, '~' = water, 'M' = miss, '!' = sunk CPU ship, 'X' = sunk human ship
 
     //ctor to initialize board
     public GameBoard() {
@@ -27,16 +26,35 @@ class GameBoard {
     }
 
     // Methods to print and update board
+    // Symbols to represent board characters
+    // '@' = HumanPlayer ships, '~' = water, 'M' = miss, '!' = sunk CPU ship, 'X' = sunk human ship
+    // 'c' = cpu ship - c will be hidden by a '~'
     public void printBoard() {
         System.out.println("        0      1      2      3      4      5      6      7      8      9");
         for (int i = 0; i < board.length; i++) {
             System.out.print(CHAR_KEYS.get(i));
             System.out.print('|');
             for (int j = 0; j < board[i].length; j++) {
-                System.out.printf("%7s", '~');
+                // checking for ships, hits, and misses to update
+                if (board[i][j] == '@' || board[i][j] == '!' || board[i][j] == 'X' || board[i][j] == 'M') {
+                    System.out.printf("%7s", board[i][j]);
+                }
+                else {
+                    System.out.printf("%7s", '~');
+                }
             }
             System.out.println();
         }
+    }
+    // Symbols to represent board characters
+    // '@' = HumanPlayer ships, '~' = water, 'M' = miss, '!' = sunk CPU ship, 'X' = sunk human ship
+    // 'c' = cpu ship - c will be hidden by a '~'
+    public void updateBoard(int row, int col, char symbol) {
+        board[row][col] = symbol;
+    }
+
+    public char[][] getBoard() {
+        return board;
     }
 
     public Map<Character, Integer> getRowKeyValues() {
@@ -51,4 +69,6 @@ class GameBoard {
             value++;
         }
     }
+
+
 }
--- test/com/vfw/game/GameBoardTest.java
+++ test/com/vfw/game/GameBoardTest.java
@@ -3,6 +3,8 @@ package com.vfw.game;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class GameBoardTest {
@@ -14,11 +16,28 @@ public class GameBoardTest {
     }
 
     @Test
-    public void testPrintBoard() {
+    public void testUpdateGameBoard_shouldVisuallyAddCharsInProperLocation() {
+        board.updateBoard(0,0, '@');
+        board.updateBoard(2,2, 'M');
+        board.updateBoard(3,6, '!');
+        board.updateBoard(7,3, 'X');
+        board.updateBoard(9,9, 'c');
+        //visually inspect A0 = @, C2 = M, D6 = !, H3 = X
+        // J9 = ~ the c should remain hidden
         board.printBoard();
     }
 
     @Test
-    public void testGetRowKeyValues() {
+    public void testPrintBoard_visuallyInspectBoardLayout() {
+        board.printBoard();
+    }
+
+    @Test
+    public void testGetRowKeyValues_shouldBeProperlyMappedLetterToValue() {
+        // A=0, B=1, C=2,.....J=9
+        Map<Character, Integer> letterKeyValue = board.getRowKeyValues();
+        assertEquals(10, letterKeyValue.size());
+        assertEquals(Integer.valueOf(0), letterKeyValue.get('A'));
+        assertEquals(Integer.valueOf(9), letterKeyValue.get('J'));
     }
 }
