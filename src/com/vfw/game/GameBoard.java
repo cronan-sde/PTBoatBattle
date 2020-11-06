db9aa4810303a6092c6349e0da04cb6bc29e01b6
Cody Cronberger <codycronberger@gmail.com>
2020-11-06 12:39:02 -0800

    Added row key map, added printBoard

--- src/com/vfw/game/GameBoard.java
+++ src/com/vfw/game/GameBoard.java
@@ -1,4 +1,42 @@
 package com.vfw.game;
 
-public class GameBoard {
+
+import java.util.*;
+
+class GameBoard {
+    private char[][] board; // 2D game board
+    private static final int SIZE = 10; // dimension of game board
+    Map<Character, Integer> rowKeyValues = new HashMap<>(10);
+
+    // Symbols to represent board characters
+    // '@' = HumanPlayer ships, '~' = water, 'M' = miss, '!' = sunk CPU ship, 'X' = sunk human ship
+
+    //ctor to initialize board
+    public GameBoard() {
+        board = new char[SIZE][SIZE];
+    }
+
+    // Methods to print and update board
+    public void printBoard() {
+        System.out.print("       0   1   2   3   4   5   6   7   8   9");
+        for (int i = 0; i < board.length; i++) {
+
+        }
+    }
+
+    public Map<Character, Integer> getRowKeyValues() {
+        return Collections.unmodifiableMap(rowKeyValues);
+    }
+
+    private void setRowKeyValues() {
+        List<Character> charKeys = new ArrayList<>(
+                Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'));
+
+        int value = 0;
+
+        for (Character key : charKeys) {
+            rowKeyValues.put(key, value);
+            value++;
+        }
+    }
 }
