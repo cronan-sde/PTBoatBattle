a945861ea0923ec44b8f515fbca4451fd272f7bc
Cody Cronberger <codycronberger@gmail.com>
2020-11-07 19:20:40 -0800

    Added functionality to Player and HumanPlayer class, specifically implemented version of the generateShot and setShips method

--- src/com/vfw/users/HumanPlayer.java
+++ src/com/vfw/users/HumanPlayer.java
@@ -1,15 +1,57 @@
 package com.vfw.users;
 
+import java.util.Scanner;
+
 //TODO: implement HumanPlayer Class
-class HumanPlayer extends Player{
+public class HumanPlayer extends Player{
+   private Scanner scan = new Scanner(System.in);
+
+   public HumanPlayer(String name) {
+      super(name);
+   }
 
    @Override
    public String setShips() {
-      return null;
+      String location = null;
+      boolean isValid = false;
+
+      while (!isValid) {
+         System.out.print("Enter the location to place the ship: ");
+         location = scan.nextLine().toUpperCase();
+
+         if (location.length() == 2) {
+            isValid = true;
+         }
+         else {
+            System.out.println(invalidFormat());
+         }
+      }
+
+      return location;
    }
 
    @Override
    public String generateShot() {
-      return null;
+      String location = null;
+      boolean isValid = false;
+
+      while (!isValid) {
+         System.out.print("Enter the location you would like to shoot at: ");
+         location = scan.nextLine().toUpperCase();
+
+         if (location.length() == 2) {
+            isValid = true;
+         }
+         else {
+            System.out.println(invalidFormat());
+         }
+      }
+      return location;
+   }
+
+   // returns message that user entered location in an invalid form
+   private String invalidFormat() {
+      return "Invalid Input - location must be in form of row letter + column number -" +
+              " e.g. A1, B2, D5 etc. Case Insensitive";
    }
 }
