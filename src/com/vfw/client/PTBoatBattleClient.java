680723166f2d617e5d3b2833337558de0cb77eb5
Cody Cronberger <codycronberger@gmail.com>
2020-11-11 00:32:03 -0800

    made minor cosmetic changes, fixing imports etc. Implemented the CPUPlayers usedLocations, so now the cpu can track every location it has previously used

--- src/com/vfw/client/PTBoatBattleClient.java
+++ src/com/vfw/client/PTBoatBattleClient.java
@@ -4,7 +4,7 @@ import com.vfw.game.PTBoatBattle;
 
 public class PTBoatBattleClient {
     public static void main(String[] args) throws InterruptedException {
-        PTBoatBattle ptbb = new PTBoatBattle();
-        ptbb.initializeGame();
+        PTBoatBattle app = new PTBoatBattle();
+        app.initializeGame();
     }
 }
